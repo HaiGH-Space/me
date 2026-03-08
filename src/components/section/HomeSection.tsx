01f938f0d034963f69d88e89429eb1e2e31c46ee
HaiGH-Space <nthai0932169055@gmail.com>
2026-03-08 17:00:07 +0700

    fix: adjust margin classes for button alignment in HomeSection

--- src/components/section/HomeSection.tsx
+++ src/components/section/HomeSection.tsx
@@ -17,7 +17,7 @@ export default function HomeSection() {
                 <h1 className="text-3xl md:text-5xl font-bold">Hi, It's <GradientText>Hai</GradientText></h1>
                 <TypingH3/>
                 <p className="max-w-lg mt-2 text-slate-300 leading-relaxed text-sm md:text-[16px]">As a programmer, I love solving problems through lines of code. I don't just write code, I build seamless digital experiences.</p>
-                <div className="text-primary flex flex-row space-x-3 items-center justify-center lg:justify-end my-4">
+                <div className="text-primary flex flex-row space-x-3 items-center justify-center lg:justify-end mt-4">
                     <Icon onClick={() => window.open("https://github.com/HaiGH-Space", "_blank")}>
                         <FaGithub size={20} />
                     </Icon>
@@ -25,7 +25,7 @@ export default function HomeSection() {
                         <FaLinkedin size={20} />
                     </Icon>
                 </div>
-                <div className="space-x-2">
+                <div className="space-x-2 my-6">
                     <a href={`${import.meta.env.BASE_URL}/Hai-Nguyen-CV.pdf`} target="_blank" rel="noopener noreferrer" className={cn(isMobile ? "text-sm px-2 py-1" : "px-4 py-2", "font-bold border-2 rounded-full hover:scale-105 transition-transform duration-300 hover:shadow-[0_0_25px_0] text-black bg-primary border-none cursor-pointer shadow-[0_0_10px_0] shadow-primary")}>
                         Resume
                     </a>
